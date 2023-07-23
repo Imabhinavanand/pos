e2df53af89fe7941b9ff24ed44f2619b70bb00d8
ABHINAV ANAND <abhinavanandiit@gmail.com>
2023-07-23 23:41:31 +0530

    code deploy

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require("colors");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://learning:GQYt8dQLYgclgYOx@cluster0.t6fgp1f.mongodb.net/");
     console.log(`MongoDB Connected ${conn.connection.host}`.bgYellow);
   } catch (error) {
     console.log(`Error : ${error.message}`.bgRed);
